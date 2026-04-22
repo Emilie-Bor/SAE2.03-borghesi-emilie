7a71061e72f1bc2d2d3c669cccdf3257e4914c01
Borghesi Emilie <borghesi1@mmi-dev.unilim.fr>
2026-04-22 16:56:52 +0200

    itération 1 buggée V2

---
+++ app/component/Movie/script.js
@@ -0,0 +1,28 @@
+let templateFile = await fetch("./component/Movie/template.html");
+let template = await templateFile.text();
+
+let templateLiFile = await fetch("./component/Movie/templateLi.html");
+let templateLi = await templateLiFile.text();
+
+let Movie = {};
+
+Movie.format = function (data, tab) {
+
+  let html = template;
+//  html = html.replaceAll("{{cssClass}}", css);
+//  html = html.replaceAll("{{nameMovie}}", data.name);
+//  html = html.replaceAll("{{imgMovie}}", "../server/images" + data.image);
+
+  let menuHTML = "";
+    for (let i of data.items) {
+        let li = templateLi;
+        li = li.replaceAll("{{nameMovie}}", data.name);
+        li = li.replaceAll("{{imgMovie}}", "../server/images" + data.image);
+        menuHTML += li;
+    }
+    html = html.replaceAll("{{itemMovie}}", menuHTML);
+
+    return html;
+};
+
+export { Movie };
