5909f01e59cd750b969eaa34848f4a2a6077daa1
Borghesi Emilie <borghesi1@mmi-dev.unilim.fr>
2026-05-04 12:35:01 +0200

    itération 6 en cours + ajout d'un lien dans le hub

--- app/component/NavBar/script.js
+++ app/component/NavBar/script.js
@@ -1,13 +1,26 @@
 let templateFile = await fetch("./component/NavBar/template.html");
 let template = await templateFile.text();
 
+let templateLiFile = await fetch("./component/NavBar/templateLi.html");
+let templateLi = await templateLiFile.text();
+
 let NavBar = {};
 
 NavBar.format = function (hAbout, hHome) {
   let html = template;
   html = html.replace("{{hAbout}}", hAbout);
   html = html.replace("{{hHome}}", hHome);
-  return html;
+
+  let menuHTML = "";
+
+  for (let item of profile) {
+    let li = templateLi;
+    li = li.replaceAll("{{profileName}}", item.name);
+
+    menuHTML += li;
+  }
+
+  return html.replaceAll("{{profileItem}}", menuHTML);;
 };
 
 export { NavBar };
